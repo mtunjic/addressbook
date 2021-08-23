caad4e37b7d114d24b940f29a403f3f293c48d0b
Marko <marko.tunjic@live.com>
2021-08-23 14:26:21 +0200

    user and devise model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,4 +7,16 @@ class Contact < ApplicationRecord
 	validates :first_name, :email, presence: true
   accepts_nested_attributes_for :addresses 
   accepts_nested_attributes_for :phones
+
+  def name_with_initial
+    "#{first_name.first}. #{last_name}"
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.by_letter(letter)
+    where("last_name LIKE ?", "#{letter}%").order(:last_name)
+  end
 end
