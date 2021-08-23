8e49f32e9bfccf92f4253c8d2e60ab940cddd5ad
Marko <marko.tunjic@live.com>
2021-08-23 14:50:27 +0200

    user validation test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "shoud normalize email" do
+    user = users(:one)
+    user.email = "FOO@mail.com"
+
+    user.send(:downcase_email)
+   
+    expected = "foo@mail.com"
+    actual = user.email
+    assert_equal(expected, actual)
+  end
 end
