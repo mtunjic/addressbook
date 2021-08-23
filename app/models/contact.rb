4eb62b9811f30b93a6c4af0deacd8beea7cee9b6
Marko <marko.tunjic@live.com>
2021-08-23 14:16:56 +0200

    added missing associations between models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,10 @@
 class Contact < ApplicationRecord
   belongs_to :user
+  belongs_to :group
+  has_many :addresses, dependent: :destroy
+  has_many :phones, dependent: :destroy
+
+	validates :first_name, :email, presence: true
+  accepts_nested_attributes_for :addresses 
+  accepts_nested_attributes_for :phones
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,3 @@
 class Group < ApplicationRecord
-  belongs_to :contact
+  has_many :contacts
 end
